e9a118d6d6a8bb3e6d42f277e6c8158e416e5fa7
Torben Jansen <torben.jansen@vizzlo.com>
2020-03-28 11:37:49 +0100

    don't use tabs

--- js/ImageLoading.js
+++ js/ImageLoading.js
@@ -14,38 +14,38 @@ let key = new Image();
 let picsToLoad = 0;
 
 function countLoadedImagesAndLaunchIfReady() {
-	picsToLoad--;
-	console.log(picsToLoad);
-	if (picsToLoad == 0) {
-		imageLoadingDoneSoStartGame();
-	}
+    picsToLoad--;
+    console.log(picsToLoad);
+    if (picsToLoad == 0) {
+        imageLoadingDoneSoStartGame();
+    }
 }
 
 function beginLoadingImage(imgVar, fileName) {
-	imgVar.onload = countLoadedImagesAndLaunchIfReady();
-	imgVar.src = fileName;
+    imgVar.onload = countLoadedImagesAndLaunchIfReady();
+    imgVar.src = fileName;
 }
 
 function loadImages() {
-	//   let dataSet = {varName: cheSprite, theFile: "che-sprite_450x400_transparent.png"}
+    //   let dataSet = {varName: cheSprite, theFile: "che-sprite_450x400_transparent.png"}
 
-	let imageList = [
-		{ varName: cheSprite, theFile: "image/che-sprite_450x400_transparent.png" },
-		{ varName: end, theFile: "image/end.png" },
-		{ varName: ground, theFile: "image/ground.png" },
-		{ varName: ground_stripe, theFile: "image/ground_stripe.png" },
-		{ varName: alpen, theFile: "image/alpen_8c.png" },
-		{ varName: key, theFile: "image/key.png" }
-	];
+    let imageList = [
+        { varName: cheSprite, theFile: "image/che-sprite_450x400_transparent.png" },
+        { varName: end, theFile: "image/end.png" },
+        { varName: ground, theFile: "image/ground.png" },
+        { varName: ground_stripe, theFile: "image/ground_stripe.png" },
+        { varName: alpen, theFile: "image/alpen_8c.png" },
+        { varName: key, theFile: "image/key.png" }
+    ];
 
-	picsToLoad = imageList.length;
+    picsToLoad = imageList.length;
 
-	for (let i = 0; i < imageList.length; i++) {
-		beginLoadingImage(imageList[i].varName, imageList[i].theFile);
-	}
+    for (let i = 0; i < imageList.length; i++) {
+        beginLoadingImage(imageList[i].varName, imageList[i].theFile);
+    }
 
-	// beginLoadingImage(end, "end.png");
-	// beginLoadingImage(ground, "ground.png");
-	// beginLoadingImage(alpen, "alpen2.png");
-	// beginLoadingImage(key, "key.png");
+    // beginLoadingImage(end, "end.png");
+    // beginLoadingImage(ground, "ground.png");
+    // beginLoadingImage(alpen, "alpen2.png");
+    // beginLoadingImage(key, "key.png");
 }
--- js/app.js
+++ js/app.js
@@ -15,103 +15,105 @@ canvasContext = canvas.getContext("2d");
 window.addEventListener("resize", resizeCanvas, false);
 
 function resizeCanvas() {
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 }
 
 window.onload = function() {
-	//  resizeCanvas();
+    //  resizeCanvas();
 
-	// canvas.height = innerHeight;
-	// canvas.width = innerWidth;
+    // canvas.height = innerHeight;
+    // canvas.width = innerWidth;
 
-	loadImages();
+    loadImages();
 };
 
 function imageLoadingDoneSoStartGame() {
-	window.requestAnimationFrame(animate);
+    window.requestAnimationFrame(animate);
 
-	let framesPerSecond = 30;
-	setInterval(function() {
-		drawAll();
-	}, 1000 / framesPerSecond);
+    let framesPerSecond = 30;
+    setInterval(function() {
+        drawAll();
+    }, 1000 / framesPerSecond);
 }
 
 function keyPressed(evt) {
-	if (evt.keyCode == 32) {
-		jump();
-	}
+    if (evt.keyCode == 32) {
+        jump();
+    }
 }
 
 function animate() {
-	let frame = Math.floor(counter % 2);
-	if (dead == true) {
-		frame = Math.floor(counter % 1);
-	}
+    let frame = Math.floor(counter % 2);
+    if (dead == true) {
+        frame = Math.floor(counter % 1);
+    }
 
-	if (highscore >= 2800) {
-		cheSprite.src = "image/angry.png";
-	}
+    if (highscore >= 2800) {
+        cheSprite.src = "image/angry.png";
+    }
 
-	//  canvasContext.drawImage(image, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
-	canvasContext.drawImage(
-		cheSprite,
-		frame * frame_width,
-		0,
-		frame_width,
-		frame_height,
-		che.x,
-		che.y + 8,
-		frame_width / 4,
-		frame_height / 4
-	);
-	counter = counter + 0.1;
-	window.requestAnimationFrame(animate);
-	motherOfFunctions();
+    //  canvasContext.drawImage(image, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
+    canvasContext.drawImage(
+        cheSprite,
+        frame * frame_width,
+        0,
+        frame_width,
+        frame_height,
+        che.x,
+        che.y + 8,
+        frame_width / 4,
+        frame_height / 4
+    );
+    counter = counter + 0.1;
+    window.requestAnimationFrame(animate);
+    motherOfFunctions();
 }
 
 function motherOfFunctions() {
-	wall1.collision();
-	wall2.collision();
-	gravity();
-	key1.moveKey();
-	gameReset(); //hmmm?
+    wall1.collision();
+    wall2.collision();
+    gravity();
+    key1.moveKey();
+    gameReset(); //hmmm?
 }
 
 function reset() {
-	highscore = 0;
+    highscore = 0;
 
-	endY = -500;
-	alpenX = 0;
+    endY = -500;
+    alpenX = 0;
 
-	key1.x = 700;
-	key1.y = 300;
+    key1.x = 700;
+    key1.y = 300;
 
-	wall1.speed = 8;
-	wall2.speed = 8;
+    wall1.speed = 8;
+    wall2.speed = 8;
 
-	wall1.x = 700;
-	wall1.collision();
-	wall2.x = wall1.x + wallDistance;
+    speedFactor = 1;
 
-	motherOfFunctions();
+    wall1.x = 700;
+    wall1.collision();
+    wall2.x = wall1.x + wallDistance;
 
-	dead = false;
-	che.jumping == false;
+    motherOfFunctions();
+
+    dead = false;
+    che.jumping == false;
 }
 
 function gameReset() {
-	if (dead == false) {
-		document.addEventListener("keydown", keyPressed);
-		// document.addEventListener("keyup", keyReleased);
-		document.removeEventListener("mousedown", reset);
-		document.addEventListener("mousedown", jump);
-		document.addEventListener("touchstart", jump);
-	}
-	if (dead == true) {
-		document.removeEventListener("mousedown", jump);
-		document.addEventListener("mousedown", reset);
-	}
+    if (dead == false) {
+        document.addEventListener("keydown", keyPressed);
+        // document.addEventListener("keyup", keyReleased);
+        document.removeEventListener("mousedown", reset);
+        document.addEventListener("mousedown", jump);
+        document.addEventListener("touchstart", jump);
+    }
+    if (dead == true) {
+        document.removeEventListener("mousedown", jump);
+        document.addEventListener("mousedown", reset);
+    }
 }
 
 // if (highscore >= thisHighscore) { noch AUFRÄUMEN
