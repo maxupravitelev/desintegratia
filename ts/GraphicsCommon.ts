ff1f68e120bd8670651df00b6c7a1f81953bb82c
Max Upravitelev <max@upravitelev.de>
2020-08-14 13:07:45 +0200

    make fillColor-argument be optional in colorRect func

--- ts/GraphicsCommon.ts
+++ ts/GraphicsCommon.ts
@@ -1,4 +1,4 @@
-const colorRect = (topLeftX, topLeftY, boxWidth, boxHeight, fillColor) => {
+const colorRect = (topLeftX, topLeftY, boxWidth, boxHeight, fillColor?) => {
     canvasContext.fillStyle = fillColor;
     canvasContext.fillRect(topLeftX, topLeftY, boxWidth, boxHeight);
 }
