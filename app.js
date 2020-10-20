9348998dc69cccf668d5ecbc7f5f7eb4e043a338
Max Upravitelev <max@upravitelev.de>
2020-10-20 13:38:00 +0200

    add requests

---
+++ app.js
@@ -0,0 +1,25 @@
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
+const express = require('express');
+require('express-async-errors');
+const app = express();
+const cors = require('cors');
+const scoreRouter = require('./controllers/scores');
+const mongoose = require('mongoose');
+logger.info('connecting to', config.MONGODB_URI);
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+    logger.info('connected to MongoDB');
+})
+    .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message);
+});
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
+app.use(middleware.requestLogger);
+app.use('/api/scores', scoreRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+module.exports = app;
